1171b4c53b4b691fc7609b85bd13450bbd10ba5e
Admin5152 <sethagyeimensah2@gmail.com>
2025-10-27 16:09:37 +0000

    Windsurf3

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/amanabi-stock-tracker/",
-
+    base: "/login/",
   server: {
     host: "::",
     port: 8080,
